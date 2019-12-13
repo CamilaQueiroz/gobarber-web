cab85bd133e432e00780844dcf60c3019cf8da50
camilaqueiroz <camilaqueiroz@mbpdemacbook2.home>
2019-12-13 17:48:44 -0300

    Cadastro na aplicação

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -12,6 +12,20 @@ export function singInSuccess(token, user) {
  };
 }
 
+export function singUpRequest(name, email, password) {
+ return {
+  type: '@auth/SIGN_UP_REQUEST',
+  payload: { name, email, password },
+ };
+}
+
+export function singUpSuccess(token, user) {
+ return {
+  type: '@auth/SIGN_UP_SUCCESS',
+  payload: { token, user },
+ };
+}
+
 export function signFailure() {
  return {
   type: '@auth/SIGN_FAILURE',
