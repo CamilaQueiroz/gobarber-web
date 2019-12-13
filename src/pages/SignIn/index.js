d97366f4f2d542675498ca4fa7bd0ff2fb1391c4
camilaqueiroz <camilaqueiroz@mbpdemacbook2.home>
2019-12-13 12:31:54 -0300

    Autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { singInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+ email: Yup.string()
+  .email('Insira um email válido')
+  .required('O email é obrigatório'),
+ password: Yup.string()
+  .min(6, 'A senha deve ter pelo menos 6 caracteres')
+  .required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
- return <div>SignIn</div>;
+ const dispatch = useDispatch();
+
+ function handleSubmit({ email, password }) {
+  dispatch(singInRequest(email, password));
+ }
+ return (
+  <>
+   <img src={logo} alt="GoBarber" />
+
+   <Form schema={schema} onSubmit={handleSubmit}>
+    <Input name="email" type="email" placeholder="Seu e-mail" />
+    <Input name="password" type="password" placeholder="Sua senha secreta" />
+
+    <button type="submit">Acessar</button>
+    <Link to="/register">Criar conta gratuita</Link>
+   </Form>
+  </>
+ );
 }
